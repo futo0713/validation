1c1a89252a7a4b779a71862b67d44e650d16ec33
FUTOSHI <36586840+futo0713@users.noreply.github.com>
2019-05-18 10:33:04 -0400

    Add files via upload

--- CNN1.py
+++ CNN1.py
@@ -1,8 +1,20 @@
+import function1
 import numpy as np
-import sys
+import time
+
+#minist_load
+save_file = '/Users/tsutsumifutoshishi/Desktop/MNIST_test/mnist.pkl'
+X_train,T_train,X_test,T_test = function1.mnist(save_file)
+
+# function1.show(X_train,T_train,60)
 
 #-----------------------------------------------------
-# Feature Map画像の大きさ
-def FM_size(img,F_size):
-    return int(np.sqrt(img.shape[1])-F_size+1)
+#initial settings
+F_size = 5
+F = np.random.randn(F_size,F_size)
 
+#-----------------------------------------------------
+#iteration
+num_of_itr=1
+for i in range(num_of_itr):
+    print(F)
